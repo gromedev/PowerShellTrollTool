95ac4f245f0d37a764abbc773d64683832ec15db
Thomas Martin Grome <tmg@venzo.com>
2022-08-24 23:32:17 +0200

    ok

--- Public/Get-DefenderPayloadTests.ps1
+++ Public/Get-DefenderPayloadTests.ps1
@@ -1,15 +1,18 @@
-﻿function Get-DefenderPayloadTests
+﻿#.DESCRIPTION 
+#    Fetches a collection of non-malignant ASR and Defender payloads to C:\temp\Fetch-DefenderPayloadTests
+function Get-DefenderPayloadTests
 {
-    Write-Host = "Fetch a collection of non-malign ASR and Defender payloads to C:\temp\Fetch-DefenderPayloadTests"
-    $result = [System.Windows.Forms.MessageBox]::Show('These files WILL set your A/V off. Continue?' , "WARNING!" , 4)
-    
-    if ($result -eq 'Yes') {
-    While (1) 
-        { 
-            $Path = "C:\temp\DefenderPayloadTests.zip"
-            $Payloads  = Invoke-WebRequest -uri "https://github.com/gromedev/PowerShellTrollTool/blob/main/Assets/Get-DefenderPaylodTests.zip" -Outfile $Path | 
-            #Expand-Archive -Path $Path -DestinationPath "C:\Temp\Get-DefenderPayloadTests"
-            explorer.exe "C:\Temp\"
-        }     
+    $Path = "C:\temp\DefenderPayloadTests.zip"
+    $Payloads  = Invoke-WebRequest -uri "https://github.com/gromedev/PowerShellTrollTool/blob/main/Assets/Get-DefenderPaylodTests.zip" -Outfile $Path 
+
+    $result = [System.Windows.Forms.MessageBox]::Show('Extracting these files WILL set your A/V off. Continue?' , "Extract zip?" , 4)
+    if ($result -eq 'Yes') 
+    {
+        Expand-Archive -LiteralPath $Path -DestinationPath "C:\Temp\Get-DefenderPayloadTests"
+        explorer.exe "C:\Temp\"
     }
+    else 
+    {
+        explorer.exe "C:\Temp\"
+    }     
 }
